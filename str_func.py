158059e6b31373678a6aee4a619e1d993f97f5d4
pnikulin <pnnikulin@gmail.com>
2023-05-08 15:34:46 +0300

    conflict solved

--- str_func.py
+++ str_func.py
@@ -1,7 +1,9 @@
 def string_upper(value):
-    """Func for upper letters"""
+    """Func for upper letters
+    """
     return value.upper()
 
 def string_upper_first_letter(value):
-    """Func for upper first letter"""
+    """Func for upper first letter
+    """
     return value.title()
