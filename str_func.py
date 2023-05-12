3cb23e2c4774a39857dd5acdacb93c19085feb4e
pnikulin <pnnikulin@gmail.com>
2023-05-12 12:48:50 +0300

    added second func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def string_upper(value):
+    """Func for upper letters"""
     return value.upper()
+
+
+def string_upper_first_letter(value):
+    """Func for upper first letter"""
+    return value.title()
